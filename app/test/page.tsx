e24eda418e5687a568fd74f2861698ddbb16b2fb
GSDV <sdvgab@gmail.com>
2024-08-02 22:42:38 -0700

    new link

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -6,15 +6,13 @@ import { useEffect, useState } from 'react';
 export default function Page() {
     const [loading, setLoading] = useState<boolean>(true);
     const [pastImage, setPastImage] = useState<File | null>(null);
-    // const [blobb, setBlob] = useState<Blob | null>(null);
 
     const test = async () => {
-        const response = await fetch("https://buyillini.s3.us-east-2.amazonaws.com/post-f-38fd96f0-d314-44cc-a5a2-d67f48520c57");
+        const response = await fetch("https://buyillini.s3.us-east-2.amazonaws.com/post-f-6feca0a1-8faf-4b54-b8a8-cf1f276c9ac1");
         const blob = await response.blob();
         const file = new File([blob], `image`, { type: blob.type });
         setPastImage(file);
         setLoading(false);
-        // setBlob(blob)
     }
 
     useEffect(() => {
